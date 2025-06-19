1de1f639c676cadf83c34f541ca652a58059a39c
Juan <11713137+jrusco@users.noreply.github.com>
2025-06-19 09:00:45 -0300

    feat: Initialize Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/com/jrusco/thread_pool_executer/ThreadPoolExecuterApplication.java
@@ -0,0 +1,13 @@
+package com.jrusco.thread_pool_executer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadPoolExecuterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadPoolExecuterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrusco/thread_pool_executer/ThreadPoolExecuterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrusco.thread_pool_executer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadPoolExecuterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
